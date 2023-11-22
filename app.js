65b7d11e67ae5c5ae7b3742a2b63149a46d15787
Hasna08 <hasina08@gmail.com>
2023-11-22 20:12:56 +0300

    Premiere ajout sur github

---
+++ app.js
@@ -0,0 +1,10 @@
+var burger=document.querySelector('.burger');
+const nav=document.querySelector(".nav-principal")
+
+burger.addEventListener('clik',()=>{
+    burger.classList.toggle('active');
+    nav.classList.toggle("active");
+}
+)  
+
+
