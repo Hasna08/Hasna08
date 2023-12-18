1b99187a6a5aa8c0995537121859880059da4fc1
isalotra <isalotra.kikia@gmail.com>
2023-12-18 10:43:53 +0300

    manu

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
  
 const menu = document.querySelector(".nav-principal");
 const btn = document.querySelector(".burger");
+const acceuil = document.querySelector('.prest');
 
-btn.addEventListener("click",()=>{
-    menu.classList.toggle("active")
-})
+acceuil.style.opacity = 1;
+menu.style.opacity = 0;
+btn.addEventListener('click', function(){
+    if(acceuil.style.opacity == 1 && menu.style.opacity == 0){
+        acceuil.style.opacity = 0;
+        menu.style.opacity = 1;
+    }
+    else{
+        acceuil.style.opacity = 1;
+        menu.style.opacity = 0;
+    }
+});
